0d439d9e0e75b913ff3063efdece4c5e5f6c896e
LFZanatta <luiszanuto@hotmail.com>
2023-12-27 07:06:05 -0300

    React front was forgot

--- backend/src/routes/Invoice.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const invoiceController = require('../controllers/InvoiceController');
-
-router.get('/api/invoiceData', invoiceController.getInvoiceData);
-router.get('/download/:filename', invoiceController.downloadFile);
-
-module.exports = router;
